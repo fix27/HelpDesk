d866c84519e72384077632caf03127592ec181b9
Князьков Максим <fix0007@mail.ru>
2017-10-11 21:24:26 +0500

    Обновление NH до 5.0

--- HelpDesk.DataService/Query/RequestLastEventQuery.cs
+++ HelpDesk.DataService/Query/RequestLastEventQuery.cs
@@ -20,10 +20,10 @@ namespace HelpDesk.DataService.Query
         public IEnumerable<RequestEventDTO> Run(IQueryable<RequestEvent> events)
         {
             //ВНИМАНИЕ!!! Запрос для ids сразу материализуется при помощи ToList(), 
-            //так как иначе NH не может его нормально преобразовать в sql
+            //так как иначе NH < 5.0 не может его нормально преобразовать в sql
 
 
-            /*IEnumerable<long> ids = (from z in events
+            IEnumerable<long> ids = (from z in events
                                      where requestIds.Contains(z.RequestId) && z.StatusRequest.Id != (long)RawStatusRequestEnum.DateEnd
                                      group z by z.RequestId into g
                                      select g.Max(d => d.Id)).ToList();
@@ -37,23 +37,23 @@ namespace HelpDesk.DataService.Query
                         Note = e.Note,
                         Transfer = e.StatusRequest.Id == (long)RawStatusRequestEnum.ExtendedDeadLine,
                         OrdGroup = e.OrdGroup
-                    };*/
+                    };
 
-            IEnumerable<long> ids = events
-                .Where(z => requestIds.Contains(z.RequestId) && z.StatusRequest.Id != (long)RawStatusRequestEnum.DateEnd)
-                .GroupBy(z => z.RequestId)
-                .Select(z => z.Max(d => d.Id)).ToList();
+            //IEnumerable<long> ids = events
+            //    .Where(z => requestIds.Contains(z.RequestId) && z.StatusRequest.Id != (long)RawStatusRequestEnum.DateEnd)
+            //    .GroupBy(z => z.RequestId)
+            //    .Select(z => z.Max(d => d.Id)).ToList();
 
 
-            var q = events.Where(e => ids.Contains(e.Id))
-                .Select(e => new RequestEventDTO
-                {
-                    RequestId = e.RequestId,
-                    DateEvent = e.DateEvent,
-                    Note = e.Note,
-                    Transfer = e.StatusRequest.Id == (long)RawStatusRequestEnum.ExtendedDeadLine,
-                    OrdGroup = e.OrdGroup
-                });
+            //var q = events.Where(e => ids.Contains(e.Id))
+            //    .Select(e => new RequestEventDTO
+            //    {
+            //        RequestId = e.RequestId,
+            //        DateEvent = e.DateEvent,
+            //        Note = e.Note,
+            //        Transfer = e.StatusRequest.Id == (long)RawStatusRequestEnum.ExtendedDeadLine,
+            //        OrdGroup = e.OrdGroup
+            //    });
 
 
             return q.ToList();
