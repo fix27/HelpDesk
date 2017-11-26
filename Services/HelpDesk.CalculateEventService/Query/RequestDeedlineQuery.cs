30955065c7c075663b29a9951a49a7cb5ce920ac
knyazkov-ma <fix0007@mail.ru>
2017-11-26 15:33:59 +0500

    Продолжение разработки

--- Services/HelpDesk.CalculateEventService/Query/RequestDeedlineQuery.cs
+++ Services/HelpDesk.CalculateEventService/Query/RequestDeedlineQuery.cs
@@ -2,6 +2,7 @@
 using HelpDesk.Entity;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace HelpDesk.CalculateEventService.Query
 {
@@ -10,13 +11,11 @@ namespace HelpDesk.CalculateEventService.Query
     /// </summary>
     public class RequestDeedlineQuery : IQuery<IEnumerable<long>, Request>
     {
-        /*if (Object.ObjectType.DeadlineHour > 0 && DateEndPlan.HasValue && !DateEndFact.HasValue)
-                    return DateEndPlan.Value <= DateTime.Now.AddHours(Object.ObjectType.DeadlineHour) && DateEndPlan.Value > DateTime.Now;*/
-
         public IEnumerable<long> Run(IQueryable<Request> requests)
         {
             var q = requests
-                .Where(r => r.Id > 0)
+                .Where(r => r.Object.ObjectType.DeadlineHour > 0 && !r.DateEndFact.HasValue &&
+                    r.DateEndPlan <= DateTime.Now.AddHours(r.Object.ObjectType.DeadlineHour) && r.DateEndPlan > DateTime.Now)
                 .Select(r => r.Id);
             
             return q.ToList();
